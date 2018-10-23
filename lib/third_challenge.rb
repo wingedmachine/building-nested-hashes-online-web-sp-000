2a97141ef5c8902078ddc0ecda064739ba0458e1
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 00:42:24 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,22 +6,27 @@ def third_challenge
         age: {}
         },
       :matriarch => {
-        #your key/value pairs here
+        name: {},
+        age: {}
         },
       :hero => {
-        #your key/value pairs here
+        name: {},
+        age: {}
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: {},
+        age: {}
         },
       :matriarch => {
-        #your key/value pairs here
+        name: {},
+        age: {}
         },
       :heroine => {
-        #your key/value pairs here
+        name: {},
+        age: {}
         },
       :heroine_friends => []
    }
